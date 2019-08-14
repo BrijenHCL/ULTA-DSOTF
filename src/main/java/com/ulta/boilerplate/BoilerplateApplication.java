2b2307458aa2c61b142a2758a2454b7cd41b00f8
BrijendraK <BrijendraK@LP-5CD8471476.HCLT.CORP.HCL.IN>
2019-08-15 00:24:17 +0530

    Initial MS as boilerplate.

---
+++ src/main/java/com/ulta/boilerplate/BoilerplateApplication.java
@@ -0,0 +1,19 @@
+/*
+ * Copyright (C) 2019 ULTA
+ * http://www.ulta.com
+ * BrijendraK@ulta.com
+ * All rights reserved
+ */
+package com.ulta.boilerplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoilerplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoilerplateApplication.class, args);
+	}
+
+}
