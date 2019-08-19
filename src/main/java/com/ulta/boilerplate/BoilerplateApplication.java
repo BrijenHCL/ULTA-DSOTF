65bf475937ce1f6488b1b186150d3f121f723b5a
BrijendraK <BrijendraK@LP-5CD8471476.HCLT.CORP.HCL.IN>
2019-08-19 18:19:34 +0530

    Added the dependency for Hystrix-CircuitBreaker

--- src/main/java/com/ulta/boilerplate/BoilerplateApplication.java
+++ src/main/java/com/ulta/boilerplate/BoilerplateApplication.java
@@ -8,7 +8,14 @@ package com.ulta.boilerplate;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
+
+@EnableHystrix
+@EnableHystrixDashboard
+@EnableCircuitBreaker
 @SpringBootApplication
 public class BoilerplateApplication {
 
